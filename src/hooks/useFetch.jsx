296f4fd0744ae9c4f6d28f1f67331dfcef836c8f
doriel1121 <doriel494@gmail.com>
2023-11-30 16:51:41 +0200

    host changed

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export function useFetch() {
-  const url = "http://localhost:3000";
+  const url = "https://tiny-gray-tortoise-robe.cyclic.app";
   const FetchData = (path, type, payload = null) => {
     return type === "get"
       ? axios.get(url + path, { withCredentials: true })
